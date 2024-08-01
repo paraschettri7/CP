65794795e69e508cf0e02c6988e9e7741ec432e1
paraschettri7 <chettriparas7@gmail.com>
2024-08-01 15:46:05 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,91 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define pb push_back
-#define YES { cout<<"YES\n"; }
-#define NO { cout<<"NO\n"; }
+using ll = long long;
+using vll =vector<ll>;
+using pi= pair<int,int>;
 #define gcd(a,b) __gcd(a,b)
 #define lcm(a,b) (a*(b/gcd(a,b)))
-#define forloop(i,n) for(int i=0; i<n; i++)
-#define forlooponebased(i,n) for(int i=1; i<=n; i++)
-#define forlooptilln(i,n) for(int i=0; i<=n; i++)
-#define reverseforloop(i,n) for(int i=n-1; i>=0; i++)
-#define debug(x) cout << #x << " = " << x << endl;
-int binary_search(vector<int>& nums, int target) {
-    int left = 0, right = nums.size() - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) return mid;
-        else if (nums[mid] < target) left = mid + 1;
-        else right = mid - 1;
-    }
-    return -1; 
-}
-const int N=1e5+10;
+#define pb push_back
+#define delete pop_back
+#define f(i,n) for(ll i=0; i<n; i++)
+#define fr(i,a,n) for (ll i = a; i < n; i++)
+#define fx(i,b,c) for(ll i=b; i>=c; i--)
+//STL FUNCTIONS
 
-// vector<int> vec1(5, 0); // Initialized with size 5 and initial value 0
-// vector<int> vec4(vec3.begin(), vec4.end()); // Initialized by copying another vector
-// vector<int> vec5;  //Initialized without default size
-// vec5.push_back(1);
-// vec5.push_back(2);
-//  vec5.push_back(3);  
-// pushback se end main element add hota hain
-//vec.erase(vec.begin() + 2); // Erase element at index 2 (3rd element)
-//vec.erase(vec.begin() + 1, vec.begin() + 4); // Erase elements from index 1 to 3
-//vec.pop_back(); // Remove the last element
-// vector<int> vec; //agar size initaialised nahi  hain toh aisa
-// int n;
-// cin>>n;
-//  forloop(i,n){
-//   int x;
-//   cin>>x;
-//   vec.pushback(x); }
-// for(auto it:vec)
-// auto it=upperbound(v.begin( ),v.end( ),element jiska bound dhundna hain)
-//result = (condition) ? value_if_true : value_if_false;
-//int result = (x > 5) ? 100 : 200;
-
-
+//int sum = accumulate(v.begin(), v.end(), 0LL);
+//int product = accumulate(v.begin(), v.end(), 1LL, multiplies<int>());
+//int x=*max_element(v.begin( ),v.end( ));
 
 
 int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	
 	int t;
 	cin>>t;
 	while(t--){
-	    int n,k;
-        cin>>n>>k;
-        vector<int> v(n);
-        forloop(i,n){
-            cin>>v[i];
-        }
-        int flag=0;
-        for(int i=1;i<n;i++){
-            if(v[i-1]>v[i]){
-                flag=1;
-                break;
-            }}
-        if(flag==0){
-            cout<<"yes"<<endl;
-            continue;
-        }
-        
-        if(k>1){
-            cout<<"YES"<<endl;
-            continue;
-        
-        }
-        else{
-            cout<<"NO"<<endl;
-            continue;
-        
-        }
-	    
-	    
-	   
-	    
-	    
+		
+
+
+
+
+      
 	    
 	}
 	
 
 }
+
