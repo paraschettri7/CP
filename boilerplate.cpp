0195e94cc9b537ee50d71b84406f82190fc84b8b
paraschettri7 <chettriparas7@gmail.com>
2024-08-22 08:34:47 +0530

    Update and rename template.cpp to boilerplate.cpp

---
+++ boilerplate.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll,ll>;
+using vpll = vector<pair<ll,ll>>;
+using mll=map<ll,ll>;
+#define test ll t; cin>>t; while(t--) solve()
+#define endl "\n"
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define pb push_back
+#define f(i,n) for(ll i=0; i<n; i++)
+#define fr(i,a,n) for (ll i = a; i < n; i++)
+#define fx(i,b,c) for(ll i=b; i>=c; i--)
+//STL FUNCTIONS
+//int product = accumulate(v.begin(), v.end(), 1LL, multiplies<int>());
+//int x=*max_element(v.begin( ),v.end( ));
+//auto it = find(all(v),element);
+
+
+
+
+
+
+
+
+void solve(){
+
+
+}
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    //test;
+}
